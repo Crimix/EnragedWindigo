7672cab9ad4ad40ae2e0707d729da102a304ef03
Tristan Bendixen <angelod1981@gmail.com>
2017-11-22 15:12:20 +0100

    Tiny amount of work on the TwitterEntryForm. Getting a grasp on Vue, at least.

--- resources/assets/js/components/TwitterEntryForm.vue
+++ resources/assets/js/components/TwitterEntryForm.vue
@@ -1,18 +1,49 @@
+<style scoped>
+  .action-link {
+    cursor: pointer;
+  }
+</style>
+
 <template>
-  <div class="container">
-    //
+  <div>
+    <div v-bind:class="{ container: !isContained }">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <span>Check Twitter user</span>
+        </div>
+        <div class="panel-body">
+          <form class="form" role="form" @submit.prevent="checkUser">
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     data() {
-      return {};
+      return {
+        isContained: false
+      };
     },
 
     mounted() {},
 
     methods: {
+      checkUser() {
+        axios.get('/twitter/vue/check', {
+          params: {
+            tuser: this.form.tuser
+          }
+        })
+        .then(response => {
+          //
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
     }
   }
 </script>
