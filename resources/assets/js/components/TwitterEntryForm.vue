8ca005ae7267e198d12aa15523a99159c5c76c63
Tristan Bendixen <angelod1981@gmail.com>
2017-11-24 08:26:02 +0100

    Twitter entry Vue component taking shape and should be done very soon.

--- resources/assets/js/components/TwitterEntryForm.vue
+++ resources/assets/js/components/TwitterEntryForm.vue
@@ -13,13 +13,47 @@
         </div>
         <div class="panel-body">
           <form class="form" role="form" @submit.prevent="checkUser">
+            <div class="form-group">
+              <label class="control-label">Twitter User</label>
+              <input id="twitter-user-name" class="form-control" name="user" v-model="form.user"
+            </div>
           </form>
         </div>
       </div>
     </div>
-  </div>
 
-  <!-- Twitter verification modal -->
+    <!-- Twitter verification modal -->
+    <div class="modal fade" id="modal-twitter-verification" tabindex="-1" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+
+            <h4 class="modal-title">Twitter Verification</h4>
+          </div>
+
+          <div class="modal-body">
+            <div class="alert alert-danger" v-if="form.errors.length > 0">
+                <p><strong>Whoops!</strong> Something went wrong!</p>
+                <br>
+                <ul>
+                    <li v-for="(error, index) in form.errors" v-bind:key="index">
+                        {{ error }}
+                    </li>
+                </ul>
+            </div>
+
+
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" @click="store">Verify</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -30,6 +64,7 @@
 
         form: {
           user: "",
+          pin: "",
           errors: []
         }
       };
